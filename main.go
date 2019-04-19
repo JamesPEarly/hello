d3d35311ed9a6aeb4a598702e509b93997c2277f
jamespearly <james.early@oswego.edu>
2019-04-19 10:03:46 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Set up the router
+	r := mux.NewRouter()
+	r.HandleFunc("/jearly/hello", func(w http.ResponseWriter, r *http.Request) {
+		// Anonymous request
+		fmt.Fprintf(w, "Hello there. Who is this?\n")
+	})
+	r.HandleFunc("/jearly/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		// Get the path variable
+		vars := mux.Vars(r)
+		name := vars["name"]
+		fmt.Fprintf(w, "Hello %s!\n", name)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
